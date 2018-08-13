71345652648713212a607b60e4f0dbe9fc8378a4
Yomar Miranda <mirandayomar@gmail.com>
2018-08-13 18:03:25 -0500

    Componente Heroes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
