f1d3d0237b5c19115d6770728813bca153826825
Yomar Miranda <mirandayomar@gmail.com>
2018-08-14 15:32:34 -0500

    Obteniendo un héroe por medio del ID que viene por parametro en la URL.

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html'
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html'
 })
 export class HeroeComponent {
+    heroe: any = {};
 
-  constructor() { }
+    constructor(private _activatedRoute: ActivatedRoute,
+                private _heroesService: HeroesService
+                ) {
+        this._activatedRoute.params.subscribe(params => {
+            /**
+             * params: Es un objeto que trae toda la información enviado
+             * Para obtener un parametro en especifico lo puedo hacer
+             * de la siguiente manera: params['id']
+             */
+            const index = params['id'];
+            this.heroe = this._heroesService.getHeroe(index);
+        });
+    }
 
 }
