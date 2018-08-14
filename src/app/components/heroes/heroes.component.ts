72827f339a1eeed6060f7f4a543c28aa7521a6dc
Yomar Miranda <mirandayomar@gmail.com>
2018-08-14 13:26:34 -0500

    Creando función para enviar a la página de un héroe en especifico y mostrar los detalles.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-heroes',
@@ -9,10 +10,14 @@ export class HeroesComponent implements OnInit {
 
     heroes: Heroe[] = [];
 
-    constructor(private _heroesService: HeroesService) { }
+    constructor(private _heroesService: HeroesService, private _router: Router) { }
 
     ngOnInit() {
         this.heroes = this._heroesService.getHeroes();
+    }
+
+    verHeroe(index: number) {
+        this._router.navigate(['/heroe', index]);
     }
 
 }
