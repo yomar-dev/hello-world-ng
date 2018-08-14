573ea2ad92b8f599269e7c846e0328730c534b2a
Yomar Miranda <mirandayomar@gmail.com>
2018-08-13 19:51:42 -0500

    Obteniendo los Heroes a través de nuestro servicio.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html'
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+    heroes: any[] = [];
 
-  ngOnInit() {
-  }
+    constructor(private _heroesService: HeroesService) { }
+
+    ngOnInit() {
+        this.heroes = this._heroesService.getHeroes();
+    }
 
 }
