4c44c9cf3d46ec7c21883cef6c4d9b241ae973b6
Yomar Miranda <mirandayomar@gmail.com>
2018-08-14 19:47:07 -0500

    Mostrando los resultados de la busqueda.

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
-  selector: 'app-buscador',
-  templateUrl: './buscador.component.html'
+    selector: 'app-buscador',
+    templateUrl: './buscador.component.html'
 })
 export class BuscadorComponent implements OnInit {
+    termino: string;
+    heroes: any[] = [];
 
-  constructor() { }
+    constructor(private _activatedRoute: ActivatedRoute, private _heroesService: HeroesService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this._activatedRoute.params.subscribe(params => {
+            this.termino = params['termino'];
+            this.heroes = this._heroesService.buscarHeroe(this.termino);
+        });
+    }
 
 }
