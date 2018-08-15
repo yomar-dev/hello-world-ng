6e29d812b89e3c3367f7b026eee67150b6fd49a2
Yomar Miranda <mirandayomar@gmail.com>
2018-08-14 21:31:22 -0500

    @Output Emitiendo un evento de un componente hijo a un componente padre.

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-heroe-tarjeta',
-  templateUrl: './heroe-tarjeta.component.html'
+    selector: 'app-heroe-tarjeta',
+    templateUrl: './heroe-tarjeta.component.html'
 })
 export class HeroeTarjetaComponent implements OnInit {
-  @Input() heroe: any = {};
-  @Input() index: number;
 
-  constructor(private _router: Router) { }
+    // Propiedades que a recibir del padre
+    @Input() heroe: any = {};
+    @Input() index: number;
 
-  ngOnInit() {
-  }
+    // Evento que el padre va a estar escuchando
+    @Output() heroeSeleccionado: EventEmitter<number>;
 
-  verHeroe() {
-    this._router.navigate(['/heroe', this.index]);
-  }
+    constructor(private _router: Router) {
+        this.heroeSeleccionado = new EventEmitter();
+    }
+
+    ngOnInit() {
+    }
+
+    verHeroe() {
+        /*this._router.navigate(['/heroe', this.index]);*/
+        this.heroeSeleccionado.emit(this.index);
+    }
 
 }
