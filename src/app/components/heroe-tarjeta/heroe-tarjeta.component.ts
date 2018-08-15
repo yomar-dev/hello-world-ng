7b800e4750de426be0ad4264505708b30d6561f1
Yomar Miranda <mirandayomar@gmail.com>
2018-08-14 21:06:44 -0500

    Componente reutilizable que recibe información de un componente padre.

---
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html'
+})
+export class HeroeTarjetaComponent implements OnInit {
+  @Input() heroe: any = {};
+  @Input() index: number;
+
+  constructor(private _router: Router) { }
+
+  ngOnInit() {
+  }
+
+  verHeroe() {
+    console.log(this.index);
+    this._router.navigate(['/heroe', this.index]);
+  }
+
+}
